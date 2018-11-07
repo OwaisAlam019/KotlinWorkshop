78c9853502ed3afca689f087a386d9054414cf81
Owais Alam <owaisstar@gmail.com>
2018-11-07 12:44:06 +0500

    Input from users and null safety

--- src/variablesAndTypes.kt
+++ src/variablesAndTypes.kt
@@ -1,16 +1,14 @@
 fun main(args: Array<String>) {
 //    Defining variables
 //    Assign-once (read-only) local variable:
+    print("Please Enter a name: ")
+    var name = readLine()
+    print("please Enter Age: ")
+    val age: Int = readLine()!!.toInt() // use !! so that it should not allow any null values from input
 
-    var a: Int = 1  // immediate assignment
-    val b = 2   // `Int` type is inferred (deduce)
-    val c: Int  // Type required when no initializer is provided
-    c = 3       // deferred(put off)  assignment
-    println("value of a is : $a"+" Value of b is : $b"+" value of c is : c")
-
-    var name = "M.Ali"
-    val age: Int = 20
-    var department: String = "HR"
+    var department: String? // "?" marks a type as nullable
+    print("Enter a department name: ")
+    department = readLine()
 
     println("Name: "+name)
     println("Age : "+age)
