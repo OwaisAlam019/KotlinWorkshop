e42b4eb78430dc982813d3b995ed7dfcd28044d2
Owais Alam <owaisstar@gmail.com>
2018-11-07 17:23:49 +0500

    unary operator with pre increement

--- src/increementDecreement.kt
+++ src/increementDecreement.kt
@@ -6,8 +6,15 @@ Priorities
 4. *,/   Multiplication & division
 5. +,-   Add & subtract
 6. =     Assignment
+7. ++,-- post-increement / post-decreement
 * */
 fun main(args: Array<String>){
-  
+    var num1:Int = 10
+    var num2:Int = 20
+    var result:Int?
+
+    result = ++num1+num2
+    println("the result of pre increement is : $result")
+    
 
 }
