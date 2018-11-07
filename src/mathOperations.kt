72e077dcb7f8526dd5db1fb7e6a0b9cc89bad024
Owais Alam <owaisstar@gmail.com>
2018-11-07 15:23:24 +0500

    madd two numbers

---
+++ src/mathOperations.kt
@@ -0,0 +1,12 @@
+
+fun main(args: Array<String>){
+
+    println("Enter two numbers")
+    var num1:Float = readLine()!!.toFloat()
+    var num2:Float = readLine()!!.toFloat()
+    var sum: Float?
+
+    sum = num1 + num2
+
+    println("Sum :"+sum)
+}
