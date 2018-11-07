1e474af0d4fb4ec819d1fbaad4ed82a0260bf658
Owais Alam <owaisstar@gmail.com>
2018-11-07 17:07:41 +0500

    Priority of unary operators empty file

---
+++ src/increementDecreement.kt
@@ -0,0 +1,13 @@
+/*
+Priorities
+1. ()    paranthesis
+2. ++,-- pre-increement / pre-decreement
+3. ^     power
+4. *,/   Multiplication & division
+5. +,-   Add & subtract
+6. =     Assignment
+* */
+fun main(args: Array<String>){
+  
+
+}
