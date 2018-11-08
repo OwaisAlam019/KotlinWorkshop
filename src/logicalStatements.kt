1b6ddcf5e926f4954c7aca3aa04b8a7d7fe1ec62
Owais Alam <owaisstar@gmail.com>
2018-11-08 20:47:55 +0500

    using if else as an expression or to return a value to a variable

--- src/logicalStatements.kt
+++ src/logicalStatements.kt
@@ -1,22 +1,20 @@
 
+/*
+if branches can be blocks, and the last expression is the value of a block:
+If you're using if as an expression rather than a statement (for example,
+returning its value or assigning it to a variable),
+the expression is required to have an else branch.
+*/
 fun main(args: Array<String>){
-    // Traditional usage
     var a = 10
     var b = 20
-    var maximum = a
-    if (a < b) maximum = b
-    println("traditional usage Maximum is $maximum")
-
-    // With else
-        var maximum2: Int
-        if (a > b) {
-            maximum2 = a
-        } else {
-            maximum2 = b
-        }
-    println(" With else Maximum is $maximum2")
-    // As expression
-        val maximumExpression = if (a > b) a else b
-    println("As expression Maximum is $maximumExpression")
+    val max = if (a > b) {
+        print("Choose a")
+        a
+    } else {
+        print("Choose b")
+        b
+    }
+    println("maximum value is $max")
 
 }
