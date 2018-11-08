adc0410eb9904f98d899b75a0cfce01c9d70d5da
Owais Alam <owaisstar@gmail.com>
2018-11-08 21:00:17 +0500

    simple when as switch

--- src/logicalStatements.kt
+++ src/logicalStatements.kt
@@ -1,20 +1,16 @@
 
 /*
-if branches can be blocks, and the last expression is the value of a block:
-If you're using if as an expression rather than a statement (for example,
-returning its value or assigning it to a variable),
-the expression is required to have an else branch.
+when replaces the switch operator of C-like languages.
 */
 fun main(args: Array<String>){
-    var a = 10
-    var b = 20
-    val max = if (a > b) {
-        print("Choose a")
-        a
-    } else {
-        print("Choose b")
-        b
+    print("enter the value of x")
+    var x = readLine()!!.toInt()
+    when (x) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> { // Note the block
+            print("x is neither 1 nor 2")
+        }
     }
-    println("maximum value is $max")
 
 }
