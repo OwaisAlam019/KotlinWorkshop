fa800750bb0d48eb2e545f0e30e7383933c16a87
Owais Alam <owaisstar@gmail.com>
2018-11-08 18:25:22 +0500

    findMyAge complete

--- src/findMyAge.kt
+++ src/findMyAge.kt
@@ -1,8 +1,14 @@
+import java.util.*
 
 fun main(args: Array<String>){
     //Input
+    println("Enter your birth year please")
+    var birthYear:Int = readLine()!!.toInt()
 
     //Process
+    var currentYear = Calendar.getInstance().get(Calendar.YEAR)
+    var age:Int = currentYear - birthYear
 
     //Output
+    println("your age is $age")
 }
