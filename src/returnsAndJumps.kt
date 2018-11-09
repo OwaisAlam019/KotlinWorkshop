c9df46f4cf60467d584241c035922633d95d24b5
Owais Alam <owaisstar@gmail.com>
2018-11-09 20:25:48 +0500

    simple break statement

---
+++ src/returnsAndJumps.kt
@@ -0,0 +1,20 @@
+
+/*
+Returns and Jumps
+
+Kotlin has three structural jump expressions:
+
+    break. Terminates the nearest enclosing loop.
+    continue. Proceeds to the next step of the nearest enclosing loop.
+    return. By default returns from the nearest enclosing function or anonymous function.
+
+* */
+fun main(args:Array<String>){
+   // simple break
+
+    for (i in 1..3){
+        println("Iteration $i")
+        if(i==2)
+            break
+    }
+}
