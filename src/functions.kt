734406dd0449b7fc16abefe6e0011f92f91c18be
Owais Alam <owaisstar@gmail.com>
2018-11-14 13:14:18 +0500

    simple functions and functions with default arguments

---
+++ src/functions.kt
@@ -0,0 +1,20 @@
+fun sum(n1:Int,n2:Int): Int{
+    return n1+n2
+}
+
+fun defaultArgument(n1:Int=22,n2:Int=23){
+    println("n1 is $n1")
+    println("n2 is $n2")
+
+}
+
+fun main(args:Array<String>){
+
+    var sum = sum(1,2)
+    println("Sum is $sum");
+
+    //calling default argument Functions
+
+    defaultArgument()
+
+}
