1b34aac5a031cae23219508127628541b8cbe52b
Owais Alam <owaisstar@gmail.com>
2018-11-14 13:23:39 +0500

    with default arguments and anonymous function

--- src/functions.kt
+++ src/functions.kt
@@ -2,9 +2,10 @@ fun sum(n1:Int,n2:Int): Int{
     return n1+n2
 }
 
-fun defaultArgument(n1:Int=22,n2:Int=23){
+fun defaultArgument(n1:Int=22,n2:Int=23,qux:()->Unit){
     println("n1 is $n1")
     println("n2 is $n2")
+    qux()
 
 }
 
@@ -14,7 +15,5 @@ fun main(args:Array<String>){
     println("Sum is $sum");
 
     //calling default argument Functions
-
-    defaultArgument()
-
+    defaultArgument(){ print("Hello")}
 }
