b17961d3e788b28233f5ac5474cfbeb1b286f449
Owais Alam <owaisstar@gmail.com>
2018-11-15 15:44:56 +0500

    Immutable collections

--- src/kotlinArrays.kt
+++ src/kotlinArrays.kt
@@ -1,54 +1,24 @@
 
-/*
-* Kotlin ArrayList class is used to create a dynamic array.
- * Which means the size of ArrayList class can be increased
- * or decreased according to requirement.
- * ArrayList class provides both read and write functionalities.*/
-
-
-
 fun main(args:Array<String>){
+//    immutable collections
+    var map = mapOf<Int,String>(1 to "Owais",22 to "ahsan")// mapof is immutable so we cannot set values after declaration
+    map.get(2)
 
-    val arrayList1 = ArrayList<String>()//Creating an empty arraylist
-
-    arrayList1.add("Ali")//Adding object in arraylist
-    arrayList1.add("Raheel")
-    arrayList1.add("Anas")
-
-    println(".......print ArrayList.......")
-
-    for (i in arrayList1) {
-        println(i)
+    var list = listOf(1,2,3,4,5)
+    println("sublist "+list.subList(1,3))
+    /*Let create an example of declaring and traversing set element using setOf() function.
+    In this example we create a set of Int type non generic and another generic set of Any type.
+    */
+    val intSet = setOf(2,6,4,29,4,5)
+    val mySet: Set<Any> = setOf(2,6,4,29,4,5,"Ashu","Ajay")
+    println(".......print Int set.........")
+    for(element in intSet){
+        println(element)
     }
-
-//    with iitial capacity
-    println("size of arrayList1 = "+arrayList1.size)
-    val arrayList2 = ArrayList<Int>(5)
-    arrayList2.add(14)
-    arrayList2.add(20)
-    arrayList2.add(80)
-    println("......print ArrayList2......")
-    for (i in arrayList2) {
-        println(i)
+    println(".......print Any set.........")
+    for(element in mySet){
+        println(element)
     }
-    println("size of arrayList2 = "+arrayList2.size)
-
-
-//    gettig and setting values in arrayList
-
-    println("value at 0"+ arrayList1.get(0))
-    println("value at 0"+ arrayList1.set(0,"Rayan"))
-    println("=============Updated Array===========")
-    for (i in arrayList1) {
-        println(i)
-    }
-
-    //search
-    if(arrayList1.contains("Rayan"))
-        println("Result found")
-    else
-        println("Not Found")
-    
 
 
 }
