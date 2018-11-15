7a2cd6284c41439b7b627b663b2c2132c064f607
Owais Alam <owaisstar@gmail.com>
2018-11-15 16:12:52 +0500

    Mutable Collections

--- src/kotlinArrays.kt
+++ src/kotlinArrays.kt
@@ -1,24 +1,40 @@
 
 fun main(args:Array<String>){
-//    immutable collections
-    var map = mapOf<Int,String>(1 to "Owais",22 to "ahsan")// mapof is immutable so we cannot set values after declaration
-    map.get(2)
+//    Mutable collections
+    val arrayList: ArrayList<String> = ArrayList<String>(2)
+    var list: MutableList<String> = mutableListOf<String>()
 
-    var list = listOf(1,2,3,4,5)
-    println("sublist "+list.subList(1,3))
-    /*Let create an example of declaring and traversing set element using setOf() function.
-    In this example we create a set of Int type non generic and another generic set of Any type.
-    */
-    val intSet = setOf(2,6,4,29,4,5)
-    val mySet: Set<Any> = setOf(2,6,4,29,4,5,"Ashu","Ajay")
-    println(".......print Int set.........")
-    for(element in intSet){
-        println(element)
-    }
-    println(".......print Any set.........")
-    for(element in mySet){
-        println(element)
+    list.add("A")
+    list.add("B")
+    list.add("C")
+
+    arrayList.addAll(list)
+    println("......print ArrayList......")
+    arrayList.forEachIndexed { index, s ->
+        println("index $index and value $s")
     }
 
+    val itr = arrayList.iterator()
+    while(itr.hasNext()) {
+        println(itr.next())
+    }
+    println("size of arrayList = "+arrayList.size)
+//    =============================== Hash Maps ======================= //
+    val hashMap:HashMap<Int,String> = HashMap<Int,String>()
+    hashMap.put(1,"Pakistan")
+    hashMap.put(3,"Dubai")
+    hashMap.put(4,"China")
+    hashMap.put(2,"India")
 
+    println(".....traversing hashmap.......")
+    for(key in hashMap.keys){
+        println("Element at key $key = ${hashMap[key]}")
+    }
+
+    hashMap.replace(3,"Ashu")
+    hashMap.put(2,"Raj")
+    println(".....hashMap.replace(3,\"Ashu\")... hashMap.replace(2,\"Raj\").......")
+    for(key in hashMap.keys){
+        println("Element at key $key = ${hashMap[key]}")
+    }
 }
