a78049b4b4370a29b2461ad3a33ee1061c286e45
Owais Alam <owaisstar@gmail.com>
2018-11-17 17:17:25 +0500

    reading from a file

--- src/writingToFile.kt
+++ src/writingToFile.kt
@@ -1,9 +1,19 @@
+import java.io.FileReader
 import java.io.FileWriter
 
 fun main(args:Array<String>){
 
-    println("Enter string to write in file")
-    writeToFile(readLine()!!)
+    println("Select Read or Write operation")
+    var op:String = readLine()!!
+    when(op){
+
+       "write" -> {
+           println("Write String")
+           writeToFile(readLine()!!)
+       }
+       "read" -> readFile("random.txt")
+    }
+
 
 }
 
@@ -17,5 +27,13 @@ fun writeToFile(str:String){
     {
         println(ex.message)
     }
+}
 
+fun readFile(file:String){
+    var reader = FileReader(file)
+    var ch:Int?
+    do {
+        ch = reader.read()
+        print(ch.toChar())
+    }while (ch!=-1)
 }
