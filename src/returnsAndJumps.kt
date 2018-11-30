c41f44e73e1332bfcd560fe7f9ad28c741ae962a
Owais Alam <owaisstar@gmail.com>
2018-11-30 21:05:16 +0500

    returning to lambda with implicit label

--- src/returnsAndJumps.kt
+++ src/returnsAndJumps.kt
@@ -14,9 +14,9 @@ fun main(args:Array<String>){
 }
 
 fun foo() {
-   listOf(1, 2, 3, 4, 5).forEach lit@{
-        if (it == 3) return@lit // local return to the caller of the lambda, i.e. the forEach loop
+   listOf(1, 2, 3, 4, 5).forEach{
+        if (it == 3) return@forEach // local return to the caller of the lambda, i.e. the forEach loop
         print(it)
     }
-    println("done with explicit label")
+    println("done with implicit label")
 }
