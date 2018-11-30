5f950ce50de9910f9fdc0e6d5e11a53512ff95c3
Owais Alam <owaisstar@gmail.com>
2018-11-30 19:02:19 +0500

    using when with multiple brancehs

--- src/usingWhenStatement.kt
+++ src/usingWhenStatement.kt
@@ -1,16 +1,16 @@
 
 fun main(args:Array<String>){
-    println("Enter your marks")
-    var marks:Int = readLine()!!.toInt()
+    println("Enter your Town Name")
+    var townName:String = readLine()!!
 
-    var grade:String?
-    when(marks){
-        in 50..60 -> grade = "C grade"
-        in 61..70 -> grade ="B grade"
-        in 71..80 -> grade ="A grade"
-        in 81..100 ->grade = "A1 grade"
-        else -> grade ="Fail"
+    var postalCode = when(townName){
+        "PECHS","DHA" -> 1565
+        "Gulshan","Johar" -> 6776
+        else ->"Invalid Input"
     }
 
-    println("Your grade is $grade")
+    if(postalCode!="Invalid Input")
+        println("Your postal code is $postalCode")
+    else
+        println(postalCode)
 }
