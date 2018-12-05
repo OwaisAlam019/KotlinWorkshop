cb7a205b99cdb6eba81ac087cadea1f152b7c7c2
Owais Alam <owaisstar@gmail.com>
2018-12-05 14:19:44 +0500

    primary  constructor with initializer block

--- src/OOP/constructors.kt
+++ src/OOP/constructors.kt
@@ -5,7 +5,19 @@ package OOP
 * */
 
 class clothes (val brand:String,val type:String,val size:String) {
+    /*
+    * The primary constructor cannot contain any code.
+    * Initialization code can be placed in initializer blocks, which are prefixed with the init keyword.*/
+    var discount:Int = 0
 
+    init {
+        println("Cloth object created for brand ${brand}")
+        if (size=="small"){
+            discount = 20
+            println("You get $discount % discount")
+        }
+
+    }
         fun showDetails(){
             println("Brand $brand")
             println("Type $type")
