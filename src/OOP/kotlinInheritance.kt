91b29a064f55238f6f61284ffd03689a80975703
Owais Alam <owaisstar@gmail.com>
2018-12-18 13:41:39 +0500

    visibility modifiers

--- src/OOP/kotlinInheritance.kt
+++ src/OOP/kotlinInheritance.kt
@@ -1,6 +1,7 @@
 package OOP
 
 open class operator{
+  protected var  processName:String? = null // change modifier to public private protected
     fun sum(n1:Int,n2:Int):Int{
         return n1+n2
     }
@@ -10,7 +11,9 @@ open class operator{
 }
 
 class mulOperator():operator() {
-
+    init {
+        super.processName = "Multiply and divide"
+    }
     fun mul(n1:Int,n2:Int):Int{
         return n1*n2
     }
@@ -22,4 +25,5 @@ fun main(args: Array<String>) {
     var op = mulOperator()
     println("Sum :"+op.sum(2,3))
     println("Multiply "+op.mul(2,4))
+
 }
