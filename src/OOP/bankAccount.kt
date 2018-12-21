46c4862d87a6117bfd71aa277ac1dd4bbcc80d7e
Owais Alam <owaisstar@gmail.com>
2018-12-21 13:02:15 +0500

    overriding with bank account class

--- src/OOP/bankAccount.kt
+++ src/OOP/bankAccount.kt
@@ -3,7 +3,7 @@ package OOP
 import kotlin.math.roundToInt
 
 open class bankAccount(val accountNumber:Int,val accountName:String ){
-    var balance:Double = 0.0
+    open var balance:Double = 0.0
 
     fun deposit(amount:Double):Boolean{
         if(amount>0){
@@ -26,13 +26,25 @@ open class bankAccount(val accountNumber:Int,val accountName:String ){
 
 }
 
-class savingAccount(accountNumber: Int,accountName: String,val interesstRate:Float):
+open class savingAccount(accountNumber: Int,accountName: String,val interesstRate:Float):
         bankAccount(accountNumber,accountName){
-    fun depositInterest(){
+    open fun depositInterest(){
         val interest = this.balance * interesstRate / 100
         deposit(interest)
     }
 
+}
+
+class savingPlusAccount(accountNumber: Int,accountName: String):
+        savingAccount(accountNumber,accountName,10.00F){
+
+    init {
+        balance = 200.00
+    }
+    override fun depositInterest() {
+        val interest = balance*interesstRate/100
+        deposit(interest)
+    }
 }
 
 fun main(args: Array<String>) {
@@ -43,6 +55,14 @@ fun main(args: Array<String>) {
     println("Balance after interest: ${ac1.balance.roundToInt()}")
     ac1.withdraw(10.00)
     println("Balance after withdraw : ${ac1.balance.roundToInt()}")
+
+    println("Overriding deposit Interest")
+
+    var ac2 = savingPlusAccount(223344,"Ali")
+    ac2.deposit(100.00)
+    println("Balance after deposit ${ac2.balance}")
+    ac2.depositInterest()
+    println("Balance after depositing interest ${ac2.balance}")
 
 
 
